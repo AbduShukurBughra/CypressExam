2afe919bf5f56077666ebe441287e5eb68eff3d1
“[AbduShukur]” <[kurshadbughra@gmail.com]>
2023-05-08 22:02:57 +0300

    POM

---
+++ cypress/e2e/PageObjects/LoginPage.js
@@ -0,0 +1,35 @@
+/// <reference types='Cypress' />
+
+class LoginPage
+{
+          visit()
+          {
+                    cy.visit('https://admin-demo.nopcommerce.com/login')
+          }
+
+          fillEmail(value)
+          {
+                    const field=cy.get("input[id=Email]")
+                    field.clear()
+                    field.type(value)
+                    return this
+          }
+
+          fillPassword(value)
+          {
+                    const field=cy.get("input[id=Password]")
+                    field.clear()
+                    field.type(value)
+                    return this     
+          }
+
+          submit()
+          {
+                   const button = cy.get("button[type=submit]")
+                   button.click();   
+          }
+
+
+}
+
+export default LoginPage
